59350ed06ddd1d6dae21c4a885129dc88114ba4e
Ngoni <ngonimapfumo@hotmail.com>
2023-12-04 21:24:38 +0200

    Initial Commit

---
+++ movies-tv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movies-tv
+//
+//  Created by Ngoni Mapfumo on 4/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movies-tv/movies_tvApp.swift
@@ -0,0 +1,17 @@
+//
+//  movies_tvApp.swift
+//  movies-tv
+//
+//  Created by Ngoni Mapfumo on 4/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct movies_tvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
